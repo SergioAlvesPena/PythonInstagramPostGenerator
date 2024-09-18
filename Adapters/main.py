07f0e86b1cabcc631dc8a58b27ec8f111d9ec5d1
Sergio Pena <sergio.penaneto@gmail.com>
2024-09-18 00:23:07 -0300

    transcribes an audio into text

--- Adapters/main.py
+++ Adapters/main.py
@@ -2,6 +2,24 @@ import openai
 from dotenv import load_dotenv
 import os
 
+def openai_whisper_transcrever(caminho_audio, nome_arquivo, modelo_whisper, openai):
+    print("Estou transcrevendo com whispers...")
+
+    audio = open(caminho_audio, "rb")
+
+    resposta = openai.Audio.transcribe(
+        api_key = openai.api_key,
+        model = modelo_whisper,
+        file = audio
+    )
+
+    transcricao = resposta.text
+
+    with open(f"texto_completo_{nome_arquivo}.txt", "w", encoding= 'utf-8') as arquivo_texto:
+        arquivo_texto.write(transcricao)
+
+    return transcricao
+
 def main():
     load_dotenv()
 
@@ -13,6 +31,9 @@ def main():
 
     openai.api_key = api_openai
 
+    modelo_whisper = "whisper-1"
+
+    transcricao_completa = openai_whisper_transcrever(caminho_audio, nome_arquivo, modelo_whisper,openai)
 
 if __name__ == "__main__":
     main()
